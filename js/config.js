ee602d94819afff4f67dd38f94c32917cae8385f
Вадим Калиничев <mac@MacBook-Air--Vadim.local>
2026-07-11 22:14:51 +0300

    fix: include config.js for Supabase connection

---
+++ js/config.js
@@ -0,0 +1,4 @@
+window.SATURN_SUPABASE_CONFIG = {
+  url: 'https://nriqdggvdfqtcpjkkfbw.supabase.co',
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5yaXFkZ2d2ZGZxdGNwamtrZmJ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM3ODUzMDIsImV4cCI6MjA5OTM2MTMwMn0.qEi1Ht94tyvddOo1eDkX-wZWdbWEpZXtPs87-jsrGrk'
+};
